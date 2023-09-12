3f84746a5a1e6063fd6a8c96bb79874e0a208546
JaceksFridge <Japacz22@gmail.com>
2023-09-12 10:24:50 +0200

    adding value styles to dashboard

--- client/src/Desktop/HomeDesktop.js
+++ client/src/Desktop/HomeDesktop.js
@@ -15,13 +15,34 @@ const HomeDesktop = ({ user, userData }) => {
         <div className="home-absolute">
           <div className="static-container">
             <h1 className="home-welcome">Welcome, <span>{user ? user.username : "Guest"}</span></h1>
+            <div className="stats-container">
+              <div className="stats-left">
+                <div className="stats-total">total</div>
+                <div className="stats-change">change</div>
+              </div>
+              <div className="stats-right">
+                <div className="stats-health">health</div>
+                <div className="stats-happiness">happiness</div>
+                <div className="stats-wealth">wealth</div>
+                <div className="stats-nodo">nodo</div>
+              </div>
+            </div>
           </div>
           <div className="empty-scroll"></div>
           <div className="scrollable-container">
             <div className="card-container">
-              <div className="home-card"></div>
+              <div className="home-card one"></div>
               <div className="home-card two"></div>
               <div className="home-card three"></div>
+              <div className="home-card"></div>
+              <div className="lset">
+                <div className="logout-btn">
+                    <div className="text">logout</div>
+                </div>
+                <div className="settings-btn">
+                  <div className="text">settings</div>
+                </div>
+              </div>
             </div>
 
           </div>
