d52289e4fdd88dd7e051304ad3926ba3c0bad3f8
JaceksFridge <Japacz22@gmail.com>
2023-09-14 00:11:57 +0200

    correct schema

--- client/src/pages/GoalSettings.js
+++ client/src/pages/GoalSettings.js
@@ -4,57 +4,25 @@
 const GoalSettings = () => {
 
     const settings = {
-        healthChecks: {
-            love: 200,
-            run: 100,
-            meditation: 100,
-            journal: 50
+        health: {
+            slider: {},
+            checkers: { yoga: 100, run: 100, },
+            counters: { workout: 50, stretch: 50, walk: 50, }
         },
-        healthRepeats: {
-            walk: 50,
-            exercise: 20,
-            read: 10,
-            meals: 5,
+        wealth: {
+            slider: { title: "hours of work", scale: 10, weight: 20, },
+            checkers: { meetup: 100, },
+            counters: { learning: 50, application: 25, },
         },
-        wealthRepeats: {
-            build: 100,
-            learn: 100,
-            app: 50,
-            post: 50
+        happiness: {
+            slider: { title: "how was your day", scale: 10, weight: 20, },
+            checkers: { sex: 200, event: 100, },
+            counters: { meeting: 50, convo: 50, },
         },
-        wealthSlider: {
-            title: 'hours of work',
-            key: 'work',
-            range: 10,
-            weight: 50
-        },
-        happinessChecks: {
-            sex: 200,
-            deepcon: 200,
-            meetup: 200,
-            date: 200,
-            new: 100
-        },
-        happinessRepeats: {
-            number: 100,
-            hangout: 100,
-            chat: 50
-        },
-        happinessSlider: {
-            title: 'how was your day',
-            range: 10,
-            weight: 50
-        },
-        nodoChecks: {
-            porn: 200,
-            weed: 200,
-            alcohol: 100,
-            sugar: 100
-        },
-        nodoRepeats: {
-            caffeine: 50,
-            social: 50,
-            shows: 50
+        nodo: {
+            slider: {},
+            checkers: { weed: -200, alcohol: -100, porn: -100, sugar: -100 },
+            counters: { caffeine: -50, tvshows: -50, badfood: -50 },
         }
     }
 
