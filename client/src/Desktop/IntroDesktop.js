7ba48ba22392f1f99b0e844f5dc962f28f7cb1a7
JaceksFridge <Japacz22@gmail.com>
2023-09-26 17:01:50 +0200

    added IntroDesktop

---
+++ client/src/Desktop/IntroDesktop.js
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+
+const IntroDesktop = () => {
+  return (
+    <div className="introPage-desktop">
+        <div>Intro Page</div>
+    </div>
+  )
+}
+
+export default IntroDesktop
--- client/src/pages/IntroPage.js
+++ client/src/pages/IntroPage.js
@@ -1,12 +1,25 @@
+
+
 import React from 'react'
 import {Link, useNavigate} from "react-router-dom"
+import { useMediaQuery } from 'react-responsive'
+import IntroDesktop from '../Desktop/IntroDesktop'
 
 const IntroPage = ({bg, title, text, prog, next}) => {
 
+
   const jump = useNavigate()
+  const isDesktoporLaptop = useMediaQuery({
+    query: '(min-device-width: 1224px)'
+  })
+  
 
   return (
-    <div className="introPage">
+    <>
+      {isDesktoporLaptop ? (
+        <IntroDesktop bg={bg} title={title} text={text} prog={prog} next={next} />
+      ) : (
+      <div className="introPage">
         <div className="intro-bg">
           <img src={bg} alt="Background" className="background-image"/>
           <div className="dark-overlay">
@@ -23,19 +36,18 @@ const IntroPage = ({bg, title, text, prog, next}) => {
           </div>
         </div>
         <div className="intro-bottom">
-            <h2 className="intro-title">{title}</h2>
-            <p className="intro-text">{text}</p>
-
-            <Link
-              className="intro-button"
-              to={next}
-            >
-                <div className="arrow"></div>
-            </Link>
-        
-              
+          <h2 className="intro-title">{title}</h2>
+          <p className="intro-text">{text}</p>
+          <Link
+            className="intro-button"
+            to={next}
+          >
+            <div className="arrow"></div>
+          </Link>
         </div>
-    </div>
+      </div>
+      )}
+    </>
   )
 }
 
