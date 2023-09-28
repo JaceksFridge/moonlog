11409964f94fa0faf269eb220f494944df1cfdef
JaceksFridge <Japacz22@gmail.com>
2023-09-28 21:14:17 +0200

    bug: sidebar

--- client/src/Desktop/Dashboard/DashboardDesktop.js
+++ client/src/Desktop/Dashboard/DashboardDesktop.js
@@ -2,20 +2,40 @@
 
 import React, { useState } from 'react'
 import DashboardSidebarDesktop from './DashboardSidebarDesktop'
-
+import { motion } from 'framer-motion'
 
 const DashboardDesktop = () => {
 
 
   const [ sidebar, setSidebar ] = useState(false)
 
+  const toggleSidebar = () => {
+    setSidebar(!sidebar)
+  }
+
+  const sidebarVariants = {
+    hidden: { x: -250 },
+    visible: { x: 0 },
+    exit: { x: -250 }
+  }
+
   return (
     <div className="dashboard-desktop">
-        <div className="open-button">
+        <div className="open-button" onClick={toggleSidebar}>
           <img src="/icons/sidebarIcon.svg" alt="sidebar" />
         </div>
         { sidebar && (
-          <DashboardSidebarDesktop />
+
+          <motion.div 
+            className="sidebar-container"
+            initial="hidden"
+            animate={ sidebar ? "visible" : "exit" }
+            variants={sidebarVariants}
+            transition={{ duration: 0.5 }}
+          >
+            <DashboardSidebarDesktop />
+          </motion.div>
+          
         )}
         <div className="main">
           <h1>dashbaord desktop</h1>
