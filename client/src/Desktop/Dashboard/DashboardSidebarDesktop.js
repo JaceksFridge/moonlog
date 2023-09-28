6ad50b148d73c4ed243e78239416d5f50a54b84d
JaceksFridge <Japacz22@gmail.com>
2023-09-28 23:56:57 +0200

    editign sidebar

--- client/src/Desktop/Dashboard/DashboardSidebarDesktop.js
+++ client/src/Desktop/Dashboard/DashboardSidebarDesktop.js
@@ -17,7 +17,7 @@ const DashboardSidebarDesktop = ({ toggleSidebar }) => {
             <div className="tab chart-btn-weatlth"><p>wealth</p></div>
             <div className="tab chart-btn-happiness"><p>happiness</p></div>
             <div className="tab chart-btn-nodo"><p>nodo</p></div>
-            <div className="tab chart-btn-logs">logs</div>
+            <div className="tab chart-btn-logs"><p>logs</p></div>
           </div>
         </div>
     </div>
