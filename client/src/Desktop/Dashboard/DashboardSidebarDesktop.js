113876eec6f867c3460e2801fc5d6a4404b15568
JaceksFridge <Japacz22@gmail.com>
2023-09-28 21:57:14 +0200

    editing dashboard

--- client/src/Desktop/Dashboard/DashboardSidebarDesktop.js
+++ client/src/Desktop/Dashboard/DashboardSidebarDesktop.js
@@ -9,6 +9,12 @@ const DashboardSidebarDesktop = ({ toggleSidebar }) => {
         <div className="close-button" onClick={toggleSidebar}>
           <img src="/icons/sidebarIcon.svg" alt="sidebar" />
         </div>
+        <div className="sidebar-tabs">
+          <h5>chart tabs</h5>
+          <div className="chart-tabs">
+
+          </div>
+        </div>
     </div>
   )
 }
