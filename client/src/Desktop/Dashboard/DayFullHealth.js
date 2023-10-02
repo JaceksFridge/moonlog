493a3d820f2a7d00eb485a21ef6f213a3a89c4d4
JaceksFridge <Japacz22@gmail.com>
2023-10-02 19:00:21 +0200

    added 3rd graph

--- client/src/Desktop/Dashboard/DayFullHealth.js
+++ client/src/Desktop/Dashboard/DayFullHealth.js
@@ -1,9 +1,9 @@
 
 
 import React, { useEffect } from 'react'
-import * as am5 from "@amcharts/amcharts5";
-import * as am5percent from "@amcharts/amcharts5/percent";
-import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+import * as am5 from "@amcharts/amcharts5"
+import * as am5percent from "@amcharts/amcharts5/percent"
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated"
 
 const DayFullHealth = () => {
 
