6f37d4efcbda2163c40071551df1cb0d5f84b770
JaceksFridge <Japacz22@gmail.com>
2023-10-05 22:40:38 +0200

    row customization

--- client/src/Desktop/Dashboard/LogGrid.js
+++ client/src/Desktop/Dashboard/LogGrid.js
@@ -1,6 +1,6 @@
 
 
-import React from 'react'
+import React, { useState, useEffect, useMemo, useCallback, useRef } from 'react'
 import {AgGridReact} from 'ag-grid-react'
 
 import "ag-grid-community/styles/ag-grid.css";
@@ -9,25 +9,64 @@ import "ag-grid-community/styles/ag-theme-alpine.css";
 
 const LogGrid = () => {
 
-    const rowData = [
+
+    const gridRef = useRef()
+
+    const [rowData, setRowData] = useState([
         { make: "Ford", model: "focus", price: 40000 },
         { make: "Toyota", model: "Celica", price: 45000 },
         { make: "BMW", model: "4 series", price: 50000 },
+    ])
 
-    ]
-    const columnDefs = [
+    const [columnDefs, setColumnDefs] = useState([
         { field: "make" },
         { field: "model" },
         { field: "price" }
-    ]
+    ])
 
+    const defaultColDef = useMemo(() => ({
+        sortable: true,
+        filter: true
+    }), [])
+
+
+    const cellClickedListener = useCallback( e => {
+        console.log('cellClicked', e)
+    })
+
+    const pushMeClicked = useCallback( e => {
+        gridRef.current.api.deselectAll()
+    })
+
+    const gridOptions = {
+
+        rowClass: "super-dark",
+        getRowClass: params => {
+            if (params.node.rowIndex % 2 === 0) {
+                return 'soft-dark';
+            }
+        },
+    }
+   
+
+    useEffect(() => {
+        fetch('https://ag-grid.com/example-assets/row-data.json')
+            .then(response => response.json())
+            .then(rowData => setRowData(rowData))
+    }, [])
  
 
   return (
     <div className="log-grid">
+        <button onClick={pushMeClicked}>push me</button>
         <AgGridReact 
-            className="ag-theme-alpine"
-            style={{ height: 500 }}
+            {...gridOptions}
+            ref={gridRef}
+            className="ag-theme-alpine-dark"
+            onCellClicked={cellClickedListener}
+            defaultColDef={defaultColDef}
+            rowSelection="multiple"
+            animateRows={true}
             rowData={rowData}
             columnDefs={columnDefs}
         />
