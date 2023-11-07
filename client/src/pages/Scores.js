5f8f86de8309ce5d71840dce4bc9fe91b96b79df
JaceksFridge <Japacz22@gmail.com>
2023-11-07 10:38:15 +0100

    number spin animation

--- client/src/pages/Scores.js
+++ client/src/pages/Scores.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react'
 import { CheckMarkSVG } from '../blocks/svg'
 import { useNavigate } from 'react-router-dom'
 import { motion } from 'framer-motion'
+import { useSpring, animated } from '@react-spring/web'
 import Footer from '../blocks/Footer'
 
 const Scores = () => {
@@ -30,17 +31,32 @@ const Scores = () => {
         }
     }
 
+  
+    const Number = ({ n }) => {
+      const { number } = useSpring({
+        from: { number: 0 },
+        number: n,
+        delay: 200,
+        config: { mass: 1, tension: 20, friction: 10}
+      })
+      return (
+        <animated.div>
+          {number.to((n) => n.toFixed(0))}
+        </animated.div>
+      )
+    }
+
 
   return (
     <div className="feedback-page">
         <div className="big-box">
-            <div className="svg-container">
+            <motion.div className="svg-container">
                 <CheckMarkSVG 
-                variants={pathVariantsOG}
-                initial='hidden'
-                animate='visible'
+                    variants={pathVariantsOG}
+                    initial='hidden'
+                    animate='visible'
                 />
-            </div>
+            </motion.div>
             <div className="sub-score-list">
                 <div className="sub-score-item">
                     <div className="item-title satoshi-btn">health</div>
@@ -64,7 +80,9 @@ const Scores = () => {
             Go to Dashboard
         </div>
         <div className="total-box">
-            <h1 className="total-score">{scores.sum ? scores.sum : 0}</h1>
+            <h1 className="total-score">
+                <Number n={scores.sum ? scores.sum : 0} />
+            </h1>
             <div className="total-text">This is your total score. You did very well today.</div>
         </div>
         <div className="continue-btn"></div>
