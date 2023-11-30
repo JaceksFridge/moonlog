6a3b1720806f297b97b5c681a292e6ad6d9a3b8c
JaceksFridge <Japacz22@gmail.com>
2023-11-30 15:37:33 +0100

    score animations

--- client/src/Desktop/HomeDesktop.js
+++ client/src/Desktop/HomeDesktop.js
@@ -1,13 +1,44 @@
 
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import DashboardSidebarDesktop from './Dashboard/DashboardSidebarDesktop'
+import { SidebarLogoSVG } from '../blocks/svg';
+import Number from '../blocks/Number'
 
 const HomeDesktop = ({ user, userData, cards }) => {
 
+  // sidebar
+  const jump = useNavigate()
+  const [isCollapsed, setIsCollapsed] = useState(true);
+  const handleTabChange = (tab) => {
+    jump("/dashboard", { state: { activeTab: tab } });
+  }
+
+
+  //number 
+  const [animateMainScore, setAnimateMainScore] = useState(false);
+  const [animateChange, setAnimateChange] = useState(false);
+  const onSubScoresAnimationRest = () => {
+    setAnimateMainScore(true);
+  };
+  const onMainScoreAnimationRest = () => {
+    setAnimateChange(true);
+  };
+
   return (
     <div className="home-desktop">
+      {/* <DashboardSidebarDesktop 
+        activePageProp='forms'
+        handleTabChange={handleTabChange}
+        isCollapsed={isCollapsed}
+        setIsCollapsed={setIsCollapsed}
+      /> */}
       <div className="topbar">
-        <div className="logo-block">Moonlog</div>
+        {/* <div className="logo-block">Moonlog</div> */}
+        <div className="logo-block2" onClick={() => jump('/')}>
+          <SidebarLogoSVG />
+        </div>
       </div>
       <div className="home-main">
         <div className="home-static">
@@ -22,12 +53,25 @@ const HomeDesktop = ({ user, userData, cards }) => {
               <div className="stats-left">
                 <div className="stats-total">
                   <h4 className="stat-title">total score:</h4>
-                  <h4 className="stat-value">{userData && userData.sum ? userData.sum : "0000"}</h4>
+                  <h4 className="stat-value">
+                    {/* {userData && userData.sum ? userData.sum : "0000"} */}
+                    { !animateMainScore ? ( <Number n={0} /> ) : (
+                      <Number n={userData && userData.sum ? userData.sum : 0} onRest={onMainScoreAnimationRest}/> 
+                    )}
+                  </h4>
                 </div>
                 <div className="stats-change">
                   <h4 className="stat-title">change:</h4>
-                  <h4 className="stat-value">{userData && userData.change 
-                    ? (userData.change > 0 ? '+' : '') + userData.change : '0'}%
+                  <h4 className="stat-value">
+                  {animateChange ? (
+                    <>
+                      {userData && userData.change && userData.change > 0 ? '+' : ''}
+                      <Number n={Math.abs(userData && userData.change ? userData.change : 0)} />
+                      {'%'}
+                    </>
+                  ) : (
+                    '0%'
+                  )}
                   </h4>
                 </div>
               </div>
@@ -36,10 +80,11 @@ const HomeDesktop = ({ user, userData, cards }) => {
                   <h4 className="stat-title">sub-scores:</h4>
                   <div className="title-values">
                     <div className="sub-score-values">
-                      <h4 className="sub-score-value">{userData && userData.health ? userData.health : "000"}</h4>
-                      <h4 className="sub-score-value">{userData && userData.wealth ? userData.wealth : "000"}</h4>
-                      <h4 className="sub-score-value">{userData && userData.happiness ? userData.happiness : "000"}</h4>
-                      <h4 className="sub-score-value">{userData && userData.nodo ? (userData.nodo > 0 ? '-' : '') + userData.nodo : "000"}</h4>
+                      <h4 className="sub-score-value"><Number n={userData && userData.health ? userData.health : 0} onRest={onSubScoresAnimationRest} /></h4>
+                      <h4 className="sub-score-value"><Number n={userData && userData.wealth ? userData.wealth : 0} onRest={onSubScoresAnimationRest} /></h4>
+                      <h4 className="sub-score-value"><Number n={userData && userData.happiness ? userData.happiness : 0} onRest={onSubScoresAnimationRest} /></h4>
+                      <h4 className="sub-score-value"><Number n={userData && userData.nodo ? userData.nodo * -1 : 0} onRest={onSubScoresAnimationRest} /></h4>
+                      {/* <h4 className="sub-score-value">{userData && userData.nodo ? (userData.nodo > 0 ? '-' : '') + userData.nodo : "000"}</h4> */}
                     </div>
                     <div className="sub-score-titles">
                       <h4 className="sub-score-title">health</h4>
---
+++ client/src/blocks/Number.js
@@ -0,0 +1,27 @@
+
+import { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
+import { useSpring, animated } from '@react-spring/web'
+
+const Number = ({ n, onRest }) => {
+    const [displayNumber, setDisplayNumber] = useState(0);
+  
+    useEffect(() => {
+      setDisplayNumber(n);
+    }, [n]);
+  
+    const animationProps = useSpring({ 
+        number: displayNumber, 
+        from: { number: 0 }, 
+        config: { mass: 1, tension: 20, friction: 10},
+        onRest: onRest,
+      });
+  
+    return (
+      <animated.div>
+        {animationProps.number.to((num) => num.toFixed(0))}
+      </animated.div>
+    );
+  };
+  
+  export default Number;
